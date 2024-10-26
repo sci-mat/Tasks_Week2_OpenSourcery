53c84590477a65496ab89c667155098423a45dda
kapil-git-1226 <gangwark057@gmail.com>
2024-10-26 10:30:58 +0530

    Completed the task and renamed the file to Kapil_Gangwar

--- Kapil_Gangwar.cpp
+++ Kapil_Gangwar.cpp
@@ -21,3 +21,32 @@ Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 
 
 */
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(int x){
+    if(x < 0 ||(x%10 == 0 && x !=0)){
+        return false;
+    }
+
+    int reversedHalf = 0;
+    while(x > reversedHalf){
+        reversedHalf = reversedHalf * 10 + x % 10;
+        x /= 10;
+    }
+
+    return x== reversedHalf || x == reversedHalf/10;
+}
+
+int main(){
+    int x1 = 1221;
+    int x2 = 100;
+
+    cout<< isPalindrome (x1) <<endl; 
+    cout<< isPalindrome (x2) <<endl;
+
+    return 0; 
+    
+}
